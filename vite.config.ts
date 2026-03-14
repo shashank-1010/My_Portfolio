14eb74231c3b7b961fdfe87dd499584c3be296a4
shashank-1010 <shashank.work247@gmail.com>
2026-03-14 10:14:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "basdasdad.onrender.com",
+      "localhost",
+      ".onrender.com" // This allows all subdomains on render.com
+    ]
+  }
 });
